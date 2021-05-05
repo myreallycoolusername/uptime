0ce0602fb89f3b81b272375360554565069294f2
pikzykacheng <81399324+pikzykacheng@users.noreply.github.com>
2021-05-05 16:57:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = [""] //link project anda
+const project = [""] //nama project anda
 setInterval(function() {
             urls.forEach(url => {
-            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+            axios.get(url).then(console.log("Berhasil Login Ke " + project + ", Time: "+ Date.now())).catch(() => {});
         })
     }, 60 * 1000);
